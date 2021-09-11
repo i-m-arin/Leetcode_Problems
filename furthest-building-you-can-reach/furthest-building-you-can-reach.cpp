c007033f640e3e76d6e2966ea6252a8bd5e2f048
Arindam Majumder <imarindam2000@gmail.com>
2021-09-11 12:30:45 +0530

    Time: 139 ms (25.33%), Space: 53.8 MB (44.44%) - LeetHub

--- furthest-building-you-can-reach/furthest-building-you-can-reach.cpp
+++ furthest-building-you-can-reach/furthest-building-you-can-reach.cpp
@@ -2,27 +2,26 @@ class Solution {
 public:
     int furthestBuilding(vector<int>& height, int x, int q) {
         priority_queue<int, vector<int>, greater<int>> pq;
-        int ans = 0;
+    int ans = 0;
 
-        for(int i = 0; i < height.size() - 1; i++){
-            int diff = height[i+1] - height[i];
-            if(diff <= 0){
-                
-            }
-            else{
-                pq.push(diff);
-            }
-
+    for(int i = 0; i < height.size() - 1; i++){
+        int diff = height[i+1] - height[i];
+        // if(diff <= 0){
+        //     x += abs(diff);
+        // }
+        if(diff>0){
+            pq.push(diff);
             if(pq.size() > q){
                 x -= pq.top();
                 pq.pop();
             }
-            if(x < 0){
-                break;
-            }
-            ans++;
         }
 
-        return ans;
+        if(x < 0){
+            break;
+        }
+        ans++;
+    }
+    return ans;
     }
 };
